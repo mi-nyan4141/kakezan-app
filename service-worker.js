88c7ffc2e2bc0fcc8d1fb57e0afee689feb6d937
mi-nyan4141 <sugi-c@tg.commufa.jp>
2026-08-14 21:07:34 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const N='kakezan-v2';const A=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(N).then(c=>c.addAll(A)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==N).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
